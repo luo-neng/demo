eb0e7975a9b7e06e332fd1207509da8c002c42fa
neng <3076209387@qq.com>
2026-04-26 19:47:32 +0800

    test_16.c

---
+++ test_16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	int x;
+	printf("请输入整数f");
+	scanf("%d",&x);
+	if(x<0){
+		printf("y=-1");
+	} else if(x=0){
+		printf("y=0");
+	}else{
+		printf("y=1");
+	}
+	return 0;
+ } 
